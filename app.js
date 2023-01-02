6fd44a0b0da1868741f803b203e8d5c0837e56c4
Darrell Chaddock <webtastic.dc@gmail.com>
2023-01-01 16:05:42 -0800

    Finished most of the CSS and JS for the project

---
+++ app.js
@@ -0,0 +1,42 @@
+// create variables for the button, the input, and the task container
+const addTask = document.getElementById('add-task');
+const taskContainer = document.getElementById('task-container');
+const inputTask = document.getElementById('input-task');
+
+// create Event Listener for the + button
+addTask.addEventListener('click' , function() {
+
+    // create a div to house the task and the extra functionality of the + and check btns
+    let task = document.createElement('div');
+    //give the task a class so we can style it
+    task.classList.add('task');
+    //insert list item for the actual task itself as well as the functionality of the + and check
+    let li = document.createElement('li');
+    //for this li we want to display the text we type into the input
+    li.innerText = `${inputTask.value}`;
+    // we want to append the value inside the div 
+    task.appendChild(li);
+
+    //now we have our li so we now need to include the check and delete btns
+    let checkButton = document.createElement('button');
+    checkButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+    //include a class so we can style the check btn
+    checkButton.classList.add('checkTask');
+    task.appendChild(checkButton);
+
+    // now repeat the above code to create the delete btn
+    let deleteButton = document.createElement('button');
+    deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+    deleteButton.classList.add('deleteTask');
+    task.appendChild(deleteButton);
+
+    // we've created all of the elements we need to include in our task
+    // now we need to ensure that they appear in the browser when we try to add a task
+    if(inputTask.value === "") {
+        alert('Please enter a task');
+    } else {
+        taskContainer.appendChild(task);
+    }
+
+
+});
